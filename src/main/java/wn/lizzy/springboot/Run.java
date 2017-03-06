95801e5efe8a8c6f8342da5b9f792e9c40b196aa
nicolastsuei <cuilj1982@126.com>
2017-03-06 14:56:58 +0800

    build springboot basic frame

---
+++ src/main/java/wn/lizzy/springboot/Run.java
@@ -0,0 +1,18 @@
+package wn.lizzy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * 
+ * Run.java
+ * @author cuilj  2017年3月6日--下午2:37:26
+ *
+ */
+@SpringBootApplication
+public class Run 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(Run.class, args);
+    }
+}
